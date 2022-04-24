b82858f213b8e36043375c86f674f5401a7e592d
bzimon <bogumil.zimon@gmail.com>
2022-04-24 21:02:21 +0000

    Trained RandomForest classifier 100% accuracy

--- R/05_analysis_ii.R
+++ R/05_analysis_ii.R
@@ -1,6 +1,10 @@
 # Load libraries ----------------------------------------------------------
 
-library("tidyverse")
+library(tidymodels)
+library(tidyverse)
+library(vip)
+#Control randomness
+set.seed(9966)
 
 
 # Define functions --------------------------------------------------------
@@ -8,13 +12,61 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
 
+my_data_clean_aug = read_tsv(file = "data/03_my_data_clean_aug.tsv")
+my_data_clean_aug = my_data_clean_aug %>%
+  select(!disease_type) %>% 
+  mutate_if(is.character, as.factor) %>% 
+  mutate_if(is.factor, as.numeric) %>% 
+  mutate(classification = as.factor(classification))
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug %>% ...
+# Inspired by https://rviews.rstudio.com/2019/06/19/a-gentle-intro-to-tidymodels/
+
+my_data_clean_aug %>% 
+  group_by(classification) %>% 
+  count()
+
+#There are two special interpretations of . in a formula. 
+#The usual one is in the context of a data argument of model fitting functions and means
+#‘all columns not otherwise in the formula’: see terms.formula. In the context of
+#update.formula, only, it means ‘what was previously in this part of the formula’.
+data_split = initial_split(my_data_clean_aug, prop = 0.7)
+
+# Preprocessing------------------------------------------------------------
+
+#Transforming training data and preparing receipe for testing data
+recipe = training(data_split) %>% 
+  recipe(classification ~.) %>% #model classification(y) as the func of all other calls
+  #step_corr(all_predictors()) %>% #step corr removes variables of high correlations with each othe (dimension reduction)
+  step_center(all_predictors(), -all_outcomes()) %>% #minus means selecting all X no y (target variable)
+  step_scale(all_predictors(), -all_outcomes()) %>% #minus means selecting all X no y (target variable)
+  prep()
+print(recipe)
+
+test = recipe %>%
+  bake(testing(data_split)) 
+glimpse(test)
+
+train = juice(recipe) #training split can be found in the receip so we are using juice function to extract it
+glimpse(train)
+
+model_rf = rand_forest(trees = 100,
+                    mode = 'classification') %>% 
+           set_engine("randomForest") %>% 
+           fit(classification ~ ., 
+               data = train)
+
+model_rf %>%
+  predict(test) %>%
+  bind_cols(test) %>%
+  metrics(truth = classification, estimate = .pred_class)
+glimpse(model_rf)
 
 
+#Feature importance extraction
+#https://www.tidymodels.org/start/case-study/
+  
 # Model data
 #if we perform any kind of second analysis
 
