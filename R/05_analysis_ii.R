5a7a28e20344c27717125ed0151b3415ede90a6c
bzimon <bogumil.zimon@gmail.com>
2022-04-26 10:24:54 +0000

    Modelling functional

--- R/05_analysis_ii.R
+++ R/05_analysis_ii.R
@@ -22,20 +22,20 @@ my_data_clean_aug = read_tsv(file = "data/03_my_data_clean_aug.tsv")
 
 # Preprocessing ------------------------------------------------------------
 my_data_clean_aug = my_data_clean_aug %>%
-  select(!disease_type) %>% 
+  select(!c(Disease_type, Age_group)) %>% 
   mutate_if(is.character, as.factor)
 
 
 
-data_split = initial_split(my_data_clean_aug, prop = 0.7, strata = classification)
+data_split = initial_split(my_data_clean_aug, prop = 0.7, strata = Class)
 training_data = training(data_split)
 testing_data = testing(data_split)
 
 kfold = vfold_cv(training_data, v = 10)
 
 mdl_recipe = training(data_split) %>% 
-  recipe(classification ~.) %>% # Model classification(y) as the func of all other calls
-  step_rm(id) %>% # id should not be random it will be deleted
+  recipe(Class ~.) %>% # Model Class(y) as the func of all other calls
+  step_rm(ID) %>% # id should not be random it will be deleted
   step_range(all_numeric(), -all_outcomes()) %>% #Each column will have mean of 0
   step_dummy(all_nominal(), -all_outcomes(), one_hot = TRUE) %>% # One hot encoding all categorical attributes
   prep()
@@ -101,7 +101,7 @@ plot(knn_metrics_fold)
 
 confusion_matrix = knn_clf_tune %>% 
   collect_predictions() %>% 
-  conf_mat(classification, .pred_class) %>% 
+  conf_mat(Class, .pred_class) %>% 
   print() %>% 
   tidy() %>% 
   ggplot(aes(x = Truth,
@@ -126,9 +126,9 @@ confusion_matrix = knn_clf_tune %>%
         legend.position = "none")
 plot(confusion_matrix)
 
-knn_clf_tune %>% 
+knn_metrics = knn_clf_tune %>% 
   collect_predictions() %>% 
-  conf_mat(classification, .pred_class) %>% 
+  conf_mat(Class, .pred_class) %>% 
   summary() %>% 
   select(-detection_prevalence) %>% 
   ggplot(aes(x = reorder(.metric,
@@ -169,13 +169,13 @@ knn_clf_tune %>%
 
 
 # rf_clf = rand_forest(trees = 100) %>%
-#   set_mode('classification') %>%
+#   set_mode('Class') %>%
 #   set_engine("randomForest")
 # 
 # model_rf %>%
 #   predict(test) %>%
 #   bind_cols(test) %>%
-#   metrics(truth = classification, estimate = .pred_class)
+#   metrics(truth = Class, estimate = .pred_class)
 # 
 # rf_proba = model_rf %>%
 #             predict(test) %>%
@@ -183,7 +183,7 @@ knn_clf_tune %>%
 #             glimpse()
 # 
 # rf_proba%>%
-#   gain_curve(classification, .pred_class) %>%
+#   gain_curve(Class, .pred_class) %>%
 #   glimpse()
 # 
 # glimpse(model_rf)
@@ -194,11 +194,11 @@ knn_clf_tune %>%
 
 # Visualize data ----------------------------------------------------------
 
-my_data_clean_aug %>% ...
+#my_data_clean_aug %>% ...
 
 
 # Write data --------------------------------------------------------------
-ggsave("class_distribution.png", path = "figures" , plot = y_distr, width = 4, height = 3)
+#ggsave("class_distribution.png", path = "figures" , plot = y_distr, width = 4, height = 3)
 ggsave("knn_clf_metrics.png", path = "figures" , plot = knn_metrics, width = 6, height = 4)
 ggsave("knn_clf_metrics_fold.png", path = "figures" , plot = knn_metrics_fold, width = 6, height = 4)
 ggsave("knn_confusion_matrix.png", path = "figures" , plot = confusion_matrix, width = 6, height = 6)
