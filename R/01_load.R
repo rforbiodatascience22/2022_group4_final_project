76f1d0b6fd60f4f3fb754ed7c6ce2c183bb4f2cd
MadalinaHizanu <s213812@dtu.dk>
2022-05-02 06:57:05 +0000

    Joinning by rows

--- R/01_load.R
+++ R/01_load.R
@@ -9,17 +9,15 @@ source(file = "R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
 
-my_data_raw <- read_csv(file = "data/_raw/kidney_disease.csv")
-
+my_data_raw_ckd <- read_csv(file = "data/_raw/kidney_disease.csv")
+my_data_raw_notckd <- read_csv(file = "data/_raw/data_nonckd.csv")
 
 # Wrangle data ------------------------------------------------------------
 
 #this is we do the joining of meta data and the data
 
-my_data <- my_data_raw
-
-
-
+my_data <- rbind(my_data_raw_ckd, my_data_raw_notckd)
+  
 
 
 # Write data --------------------------------------------------------------
