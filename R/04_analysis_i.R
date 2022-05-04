f852b950432f8cbf6bf25bf00ef5e269979cb746
MartinaVojtkova <s212586@dtu.dk>
2022-05-04 14:33:13 +0000

    Coloring plots and code styling in 04_analysis

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -1,5 +1,4 @@
 # Load libraries ----------------------------------------------------------
-install.packages("hrbrthemes")
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
@@ -22,110 +21,137 @@ my_data_clean_aug = read_tsv(file = "data/03_my_data_clean_aug.tsv")
 
 #any extra transformation before modelling
 my_data_clean_aug = my_data_clean_aug %>% 
-  mutate_if(is.character, as.factor)
+  mutate_if(is.character, 
+            as.factor)
 
-#all preparation for plotting
+#Creating variables for plotting
 
 my_data_clean_aug_disease = my_data_clean_aug %>% 
-  select(ID, Diabetes, CAD, Pedal_edema, Class, Disease_no) %>%
+  select(ID, 
+         Diabetes, 
+         CAD, 
+         Pedal_edema, 
+         Class, 
+         Disease_no) %>%
   filter(Class == "ckd")
 
 
 my_data_clean_aug_infection = my_data_clean_aug %>%
-  select(ID, Age, Bacteria, WB_count, Class) %>%
+  select(ID, 
+         Age, 
+         Bacteria, 
+         WB_count, 
+         Class) %>%
   filter(Class == "ckd") 
 
 
 my_data_clean_aug_diabetes = my_data_clean_aug %>%
   select(ID,
-         Diabetes ,
-         Specific_gravity ,
-         Sugar ,
+         Diabetes,
+         Specific_gravity,
+         Sugar,
          Blood_glucose , 
          Class) %>%
   filter(Class == "ckd")
 
-NAs_progress = data.frame(step = c("Raw_data", "Replacing NAs (numerical)","Dropping NAs (categorical)"), size = c(400.001, 400, 233), variable_name = c("my_data", "my_data_clean", "my_data_clean"), percent = c(1,1,0.58))
+#Data frame for data cleaning plot
+NAs_progress = data.frame(step = c("Raw data", 
+                                   "Replacing NAs (numerical)",
+                                   "Dropping NAs (categorical)"), 
+                          size = c(400.001, 
+                                   400, 
+                                   233), 
+                          variable_name = c("my_data", 
+                                            "my_data_clean", 
+                                            "my_data_clean"), 
+                          percent = c(1,
+                                      1,
+                                      0.58))
+
 NAs_progress = as.tibble(NAs_progress)
 
 # Visualize data ----------------------------------------------------------
 
 #Cleaning progress
 
-
-
-# Reverse side
 cleaning_plot = NAs_progress  %>% 
-  mutate(step = fct_reorder(step, size)) %>%
-  ggplot(aes(x=step, 
-             y=size, 
+  mutate(step = fct_reorder(step, 
+                            size)) %>%
+  ggplot(aes(x = step, 
+             y = size, 
              fill = variable_name,
-             label=scales::percent(percent))) +
-  geom_bar(stat="identity", alpha=.6, width=.4) +
+             label = scales::percent(percent))) +
+  geom_bar(stat="identity", 
+           alpha=.6, 
+           width=.4) +
   coord_flip() +
-  xlab("") +
-  theme_bw() +
-  geom_text(nudge_y= -50,
-            color="white",
+  scale_fill_manual(values = c('#482677ff',
+                               '#36bb75ff')) +
+  geom_text(nudge_y = -50,
+            color = "white",
             size = 5,
-            fontface="bold") +
-  labs(y = "Number of observations in the dataset",
-    title = "Data cleaning process")
+            fontface = "bold") +
+  labs(x = "",
+       y = "Number of observations in the dataset",
+       title = "Data cleaning process",
+       fill = "Variable name")+
+       theme_grey(base_size = 13) 
 
+plot(cleaning_plot)
 
 ## Correlation heatmap
-
 cor_heatmap = my_data_clean_aug %>% 
-              select(!Disease_no) %>%
-              select(!Age_group) %>%
-              select(-Class ,
-                     everything()) %>% 
-              select(!Disease_type) %>% 
-              mutate_if(is.factor, as.numeric) %>%  # Converting factors to numerical on the spot
-              print() %>% 
-              cor(use = 'all.obs', 
-                  method = 'pearson') %>%
-              round(2) %>%
-              melt() %>% 
-              ggplot(aes(x = X1,
-                         y = X2,
-                         fill = value)) +
-                     geom_tile()  +
-                     geom_text(aes(label = value), 
-                               color = "white", 
-                               size = 1.7) +
-                     theme_bw()+
-                     scale_fill_viridis(alpha = 0.85) +
-                     xlab('') +
-                     ylab('') + 
-                     labs(title = "Correlation of the attributes and target variable", 
-                          fill = "Pearson Correlation") +
-                     theme(axis.text.x = element_text(angle = 45,
-                                                      vjust = 1,
-                                                      hjust = 1),
-                          axis.title.x = element_blank(),
-                          axis.title.y = element_blank(),
-                          panel.grid.major = element_blank(),
-                          panel.border = element_blank(),
-                          panel.background = element_blank())
+  select(!Disease_no) %>%
+  select(!Age_group) %>%
+  select(-Class,
+         everything()) %>% 
+  select(!Disease_type) %>% 
+  mutate_if(is.factor,
+            as.numeric) %>%  
+  print() %>%
+  cor(use = 'all.obs', 
+      method = 'pearson') %>%
+  round(2) %>%
+  melt() %>%
+  ggplot(aes(x = X1,
+             y = X2,
+             fill = value)) +
+  geom_tile() +
+  geom_text(aes(label = value), 
+                color = "white", 
+                size = 1.7) +
+  theme_bw()+
+  scale_fill_viridis(alpha = 0.8) +
+  xlab('') +
+  ylab('') + 
+  labs(title = "Correlation of the attributes with the target variable", 
+       fill = "Pearson Correlation") +
+  theme(axis.text.x = element_text(angle = 45,
+                                   vjust = 1,
+                                   hjust = 1),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
 plot(cor_heatmap)
-              
 
 ##Age distribution plot
 
 age_distribution =  my_data_clean_aug %>% 
-                    select(Age, 
-                          Age_group, 
-                          Class) %>% 
-                    ggplot(mapping = aes(x = Age_group,
-                           fill = Class)) +
-                           geom_bar(binwidth = 2) +
-                           labs( x = "Age group",
-                                 y = "count",
-                                 title = "Distribution of age grouppes for patients with/without CKD") +
-                           scale_fill_manual(values = c("#69b3a2", 
-                                                        "#404080")) +
-                    theme_bw()
+  select(Age,
+         Age_group, 
+         Class) %>% 
+  ggplot(mapping = aes(x = Age_group,
+                       fill = Class)) +
+  geom_bar(bindwidth = 2, alpha = 0.6) +
+  labs(x = "Age group",
+      y = "count",
+      title = "Distribution of age grouppes for patients with/without CKD") +
+  scale_fill_manual(values = c('#482677ff',
+                               '#36bb75ff')) +
+  theme_grey()
   
   
 
@@ -135,10 +161,10 @@ plot(age_distribution)
 
 disease_no = ggplot(my_data_clean_aug_disease, 
                     mapping = aes(x = Disease_no)) +
-                    geom_histogram()+
-                    labs(x = "Number of diseases",
-                        y = "Frequency",
-                    title = "Number of extra diseases for patients with CKD")
+  geom_histogram() +
+  labs(x = "Number of diseases",
+       y = "Frequency",
+       title = "Number of extra diseases for patients with CKD")
 
 plot(disease_no)
   
@@ -146,86 +172,91 @@ plot(disease_no)
 ##which disease is most associated
   
 find_disease =  my_data_clean_aug %>%
-                select(ID, 
-                       Diabetes, 
-                       CAD, 
-                       Pedal_edema, 
-                       Class, 
-                       Disease_no, 
-                       Disease_type) %>%
-                filter(Disease_no == 1,
-                       Class == "ckd") %>%
-                ggplot(mapping = aes(x = Disease_type)) +
-                       geom_bar() +
-                       labs(x = "Disease type",
-                            y = "Count",
-                            title = "Number of CKD patients with one other disease")
+  select(ID, 
+         Diabetes, 
+         CAD, 
+         Pedal_edema, 
+         Class, 
+         Disease_no, 
+         Disease_type) %>%
+  filter(Disease_no == 1,
+         Class == "ckd") %>%
+  ggplot(mapping = aes(x = Disease_type)) +
+  geom_bar() +
+  labs(x = "Disease type",
+       y = "Count",
+       title = "Number of CKD patients with one other disease")
+
+
 plot(find_disease)
 
 ## find correlation between wc and infection status
 
-find_infection = ggplot(my_data_clean_aug_infection , 
+find_infection = ggplot(my_data_clean_aug_infection,
                         mapping = aes(x = ID, 
-                                       y = WB_count ,
-                                       colour = Bacteria)) +
-                        geom_bar(stat = "identity", 
-                                 width=0.2) +
-                        labs(x = "Patient ID",
-                             y = "White blood cell count",
-                             title = "White blood cell count for each CKD patient, grouped by bacterial presence") +
-                        coord_flip()
+                                      y = WB_count ,
+                                      colour = Bacteria)) +
+  geom_bar(stat = "identity", 
+           width = 0.2) +
+  labs(x = "Patient ID",
+       y = "White blood cell count",
+       title = "White blood cell count for each CKD patient, grouped by bacterial presence") +
+  coord_flip()
 
 plot(find_infection)
 
 ##distribution of white blood cells count
 
 wc =  my_data_clean_aug %>%
-      select(ID,
-             Age,
-             Bacteria,
-             WB_count,
-             Class) %>% 
-      ggplot(mapping = aes(x = WB_count, 
+  select(ID,
+         Age,
+         Bacteria,
+         WB_count,
+         Class) %>% 
+  ggplot(mapping = aes(x = WB_count, 
                            fill = Class)) +
-             geom_histogram()+
-             labs(x = "White blood cells count",
-                  y = "Frequency",
-                  title = "Distribution of white blood cell count for CKD and non-CKD patients")
+  geom_histogram() +
+  labs(x = "White blood cells count",
+       y = "Frequency",
+       title = "Distribution of white blood cell count for CKD and non-CKD patients")
 
 plot(wc)
 
 ##investigate diabetes markers - correlation with sugar levels
 
-diabetes = ggplot(my_data_clean_aug_diabetes , 
-                  mapping = aes(x = Blood_glucose , 
+diabetes = ggplot(my_data_clean_aug_diabetes,
+                  mapping = aes(x = Blood_glucose, 
                                 group = Diabetes ,
-                                fill = Diabetes))+
-                  geom_density(adjust = 1.5) +
-                  facet_wrap(~Diabetes) +
-                  labs(x = "Blood glucose level (mgs/dl)",
-                       y = "Density",
-                       title = "Blood glucose for CKD patients with/without diabetes")
+                                fill = Diabetes)) +
+  geom_density(adjust = 1.5) +
+  facet_wrap(~ Diabetes) + 
+  labs(x = "Blood glucose level (mgs/dl)",
+       y = "Density",
+       title = "Blood glucose for CKD patients with/without diabetes")
 
 plot(diabetes)
 
 ##investigate red blood cell count in ckd
 
 red_blood = my_data_clean_aug %>%
-            select(ID,
-                   RB_cells,
-                   Hemoglobin, 
-                   RB_count,
-                   Anemia, 
-                   Class) %>% 
-            ggplot(mapping = aes(x = Hemoglobin, 
-                                 y = RB_count,
-                                 alpha = Anemia)) +
-                   geom_point(size = 2, color = "red") +
-                   facet_wrap(~ Class) +
-                   labs(x = "Hemoglobin level (gms)",
-                        y = "Red blood cell count (millions/cm)",
-                        title = "Relationship between hemoglobin and red blood cells count",
-                        subtitle = "in patients with/without anemia and with/without CKD")
+  select(ID,
+         RB_cells,
+         Hemoglobin, 
+         RB_count,
+         Anemia, 
+         Class) %>% 
+  ggplot(mapping = aes(x = Hemoglobin, 
+                       y = RB_count,
+                       color = Anemia)) +
+  geom_point(size = 2.5, 
+             alpha = 0.6) +
+  scale_color_manual(values = c('#482677ff',
+                                '#36bb75ff')) +
+  facet_wrap(~ Class) +
+  labs(x = "Hemoglobin level (gms)",
+       y = "Red blood cell count (millions/cm)",
+       title = "Relationship between hemoglobin and red blood cells count",
+       subtitle = "in patients with/without anemia and with/without CKD")
 
 plot(red_blood)
 
@@ -233,34 +264,41 @@ plot(red_blood)
 #plot relationship between hemoglobin and albumin grouped by classification
 
 hemo_al_plot = my_data_clean %>% 
-               select(Class,
-                      Hemoglobin,
-                      Albumin) %>% 
-               ggplot(mapping = aes(x = Albumin, 
-                                   y = Hemoglobin,
-                                   fill = Class)) +
-                      geom_boxplot()
+  select(Class,
+         Hemoglobin,
+         Albumin) %>% 
+  ggplot(mapping = aes(x = Albumin, 
+                       y = Hemoglobin,
+                       fill = Class)) +
+  scale_fill_manual(values = c('#482677ff',
+                               '#36bb75ff')) +
+                      geom_boxplot(alpha = 0.6) +
+  labs(x = "Albumin",
+       y = "Hemoglobin (gms)",
+       title = "Relationship between hemoglobin and albumin", 
+       subtitle = "Labelling: ckd = sick with chronic kidney disease, notckd = healthy",
+       fill = "Patient status")
 
 plot(hemo_al_plot)
 
+#relationship between hemoglobin and packed cell volume grouped by classification
 
-#plot relationship between hemoglobin and packed cell volume grouped by classification
-#A convention that has been adopted in medicine is to estimate haemoglobin (HB)
-#concentration as a third of packed cell volume (PCV) or vice versa. 
-
-
-hemo_pvc_plot = my_data_clean %>% 
-                select(Class, 
-                       Hemoglobin, 
-                      Packed_cell_vol) %>% 
-                ggplot(mapping = aes(x = Packed_cell_vol, 
-                                     y = Hemoglobin ,
-                                     color = Class))+
-                       geom_point() +
-                       theme_minimal()+
-                       labs(x = "Packed cell volume",
-                            y = "Hemoglobin (gms)",
-                            title = "Relationship between hemoglobin and packed cell volume")
+hemo_pvc_plot = my_data_clean %>%
+  select(Class, 
+         Hemoglobin,
+         Packed_cell_vol) %>% 
+  ggplot(mapping = aes(x = Packed_cell_vol, 
+                       y = Hemoglobin,                                     color = Class))+
+                       geom_point(size = 2.5, 
+                                  alpha = 0.6) +
+  scale_color_manual(values = c("#482677ff",
+                                "#36bb75ff")) +
+  labs(x = "Packed cell volume",
+       y = "Hemoglobin (gms)",
+       title = "Relationship between hemoglobin and packed cell volume",
+       subtitle = "Labelling: ckd = sick with chronic kidney disease, notckd = healthy",
+       color = "Patient status")+
+  theme_grey(base_size = 13)
 
 plot(hemo_pvc_plot)
 
@@ -275,21 +313,21 @@ serum_urea_plot = my_data_clean %>%
   ggplot(mapping = aes(x = Serum_creatinine, 
                        y = Blood_urea))+
   geom_point(aes(color = Class), 
-             size = 3, 
-             alpha = 0.7) +
-  geom_smooth(method = "lm", color = "darkgrey") +
-  theme_minimal()+
-  scale_x_continuous(trans='log10') +
-  scale_y_continuous(trans='log10')+
-  labs(x = "Serum creatinine",
-       y = "Blood urea",
+             size = 2.5, 
+             alpha = 0.6 ) +
+  geom_smooth(method = "lm", color = "black") +
+  scale_color_manual(values = c("#482677ff",
+                                "#36bb75ff")) +
+  scale_x_continuous(trans = 'log10') +
+  scale_y_continuous(trans = 'log10')+
+  labs(x = "Serum creatinine (mgs/dl)",
+       y = "Blood urea (mgs/dl)",
        title = "Relationship between serum creatinine and blood urea",
-       subtitle = "ckd - Chronic Kidney Disease, notckd = Healthy",
-       )
+       subtitle = "Labelling: ckd = sick with chronic kidney disease, notckd = healthy",
+       color = "Patient status") +
+  theme_grey(base_size = 13)
+
 plot(serum_urea_plot)
-
-
-#Funnel plot 
 
 
 # Write data --------------------------------------------------------------
@@ -320,29 +358,43 @@ ggsave("cor_heatmap.png",
 
 ggsave("age_distribution.png", 
        path = "figures/distributions" , 
-       plot = age_distribution)
+       plot = age_distribution, 
+       width = 8, 
+       height = 5)
 
 ggsave("disease_no_association.png",
        path = "figures/" , 
-       plot = disease_no)
+       plot = disease_no, 
+       width = 8, 
+       height = 5)
 
 ggsave ("which_disease.png" , 
         path = "figures/distributions" ,
-        plot = find_disease)
+        plot = find_disease, 
+        width = 8, 
+        height = 5)
 
 ggsave ("infection.png" ,
         path = "figures/distributions" , 
-        plot = find_infection)
+        plot = find_infection, 
+        width = 8, 
+        height = 5)
 
 ggsave ("WC.png" , 
         path = "figures/distributions" , 
-        plot = wc)
+        plot = wc, 
+        width = 8, 
+        height = 5)
 
 ggsave ("diabetes_glucose_level.png" , 
         path = "figures/distributions" , 
-        plot = diabetes)
+        plot = diabetes, 
+        width = 8, 
+        height = 5)
 
 ggsave ("redblood.png" , 
         path = "figures" ,
-        plot = red_blood)
+        plot = red_blood,
+        width = 8, 
+        height = 5)
 
